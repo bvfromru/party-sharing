614b9946144a25695bc31873cbf6b15277cd4393
Vitalii Budkin <bvfromru@gmail.com>
2023-06-21 13:12:58 +0400

    add readme and clean code

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -3,7 +3,3 @@ import { RootState } from './store';
 export const selectPeople = (state: RootState) => state.people;
 
 export const selectPurchases = (state: RootState) => state.purchases;
-
-export const selectPersonNameById = (state: RootState, id: string): string => {
-  return state.people.find((person) => person.id === id)?.name ?? 'Not Found';
-};
